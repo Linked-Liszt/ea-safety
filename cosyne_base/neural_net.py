d39762ce071249a9eda1aa471e7a35916b1e860e
unknown <18706708+Oxymoren@users.noreply.github.com>
2020-02-16 21:14:35 -0600

    Add basic NN configs

--- cosyne_base/neural_net.py
+++ cosyne_base/neural_net.py
@@ -1,3 +1,34 @@
+import numpy as np
+import torch
+import torch.nn as nn
 
 
 
+class CosyneNet(torch.nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.layers = nn.ModuleList()
+
+        for layer_config in config["layers"]:
+            self.layers.append(self.__add_layer(
+                layer_config['type'], 
+                layer_config['params'],
+                layer_config['kwargs']))
+
+        
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def extract_params(self):
+        pass
+
+    """
+    Begin Inner Facing Methods
+    """
+    
+    def __add_layer(self, layer_type, layer_params, layer_kwargs):
+        #Finds the pytorch relevant function and calls it with params and kwargs
+        return getattr(nn, layer_type)(*layer_params, **layer_kwargs)
---
+++ cosyne_base/sandbox_cosyne.py
@@ -0,0 +1,17 @@
+from neural_net import CosyneNet as cn
+import json
+import numpy as np
+import sys
+import torch
+
+config_path = sys.argv[1]
+
+with open(config_path, 'r') as config_file:
+    config_dict = json.load(config_file)
+
+test_cn = cn(config_dict)
+
+test_input = torch.from_numpy(np.random.rand(5)).float()
+
+print(test_cn)
+print(test_cn.forward(test_input))
